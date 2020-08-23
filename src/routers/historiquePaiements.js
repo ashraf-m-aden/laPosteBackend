8d0a1c7795bf1bcbfae1891870ff7134a9be8cb8
Ashraf Mohamed <41853901+nejishow@users.noreply.github.com>
2020-08-23 08:54:00 +0300

    enregistre la personn ki suprrime

--- src/routers/historiquePaiements.js
+++ src/routers/historiquePaiements.js
@@ -42,7 +42,7 @@ router.get('/allPayment', async (req, res) => { // tous les paiements
 })
 router.get('/historicPs/:id', async (req, res) => { // tous les paiements d'un utilisateur
     try {
-        const historics = await HistoricP.find({ idClient: req.params.id })
+        const historics = await HistoricP.find({ idClient: req.params.id, enabled: true })
         if (!historics) {
             return res.status(404).send("Pas de paiements pour ce client")
 
@@ -65,13 +65,18 @@ router.get('/payment/:id', async (req, res) => { // info sur un paiement precis
     }
 })
 
-router.post('/removePayment/:id', async (req, res) => { //
+router.post('/removePayment/:id',auth, async (req, res) => { //
     try {
         const historic = await HistoricP.findById({ _id: req.params.id })
         if (!historic) {
             return res.status(404).send("Pas de paiement")
         }
-        await historic.remove()
+        historic.idStaff = req.staff._id
+        historic.toDelete = true
+        historic.toModify = true
+        historic.enabled = false
+
+        await historic.save()
         return res.status(201).send(historic)
     } catch (error) {
         return res.status(500).send(error)
@@ -82,6 +87,12 @@ router.post('/removePayment/:id', async (req, res) => { //
 router.post('/changePayment', async (req, res) => {
     try {
         const hps = await HistoricP.find({})
+        await hps.forEach(async hp => {
+          hp.enabled= true
+          hp.toModify = false;
+          hp.toDelete = false
+            await hp.save()
+        });
         await hps.forEach(async hp => {
             if (hp.boiteNumber === '' || hp.boiteNumber === null || hp.boiteNumber === undefined) {
                 hp.enabled = false
