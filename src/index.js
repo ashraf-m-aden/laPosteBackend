ffbe24972ee54db0d8104f7826c333b4ea592db3
nejishow <ashrafmuhamad2014@gmail.com>
2021-04-20 11:20:06 +0300

    change cors configuration

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ let workers = [];
 const app = express()
 const port = process.env.PORT || 3000
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://lapostededj.web.app");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With,x-requested-width, Authorization,  Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "GET, PATCH, DELETE, POST");
   next();
