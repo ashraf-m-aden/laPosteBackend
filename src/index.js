6651ea85a013bf1d9f76ffc40ed9577186adaa15
nejishow <ashrafmuhamad2014@gmail.com>
2021-04-20 12:16:29 +0300

    add corsconfig

--- src/index.js
+++ src/index.js
@@ -19,17 +19,17 @@ let workers = [];
 
 const app = express()
 const port = process.env.PORT || 3000
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With,x-requested-width, Authorization,  Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods", "GET, PATCH, DELETE, POST, OPTIONS");
-  next();
-});
-const imagesPath = path.join(__dirname, '../images')
-const configcORS = {
-  'origin': '*'
-}
-app.use(cors(configcORS))
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With,x-requested-width, Authorization,  Content-Type, Accept");
+//   res.header("Access-Control-Allow-Methods", "GET, PATCH, DELETE, POST, OPTIONS");
+//   next();
+// });
+// const imagesPath = path.join(__dirname, '../images')
+// const configcORS = {
+//   'origin': '*'
+// }
+app.use(cors())
 app.use('/images', express.static(imagesPath))
 app.use(express.json())
 app.use(clientTRouter)
