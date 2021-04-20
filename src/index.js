44c73766610a9ef9fcf13ed571cc859be8648216
nejishow <ashrafmuhamad2014@gmail.com>
2021-04-20 11:56:06 +0300

    change cors configuration

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ const port = process.env.PORT || 3000
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With,x-requested-width, Authorization,  Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods", "GET, PATCH, DELETE, POST");
+  res.header("Access-Control-Allow-Methods", "GET, PATCH, DELETE, POST, OPTIONS");
   next();
 });
 const imagesPath = path.join(__dirname, '../images')
@@ -30,7 +30,7 @@ const configcORS = {
   'origin': '*'
 }
 app.use(cors())
-
+app.options('*', cors()); // include before other routes
 app.use('/images', express.static(imagesPath))
 app.use(express.json())
 app.use(clientTRouter)
